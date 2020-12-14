72bb73b90e9313b26596c5c544ebf89aa8f2f768
Kasunath-Lakmal <50392119+Kasunath-Lakmal@users.noreply.github.com>
2020-12-14 13:51:48 +0530

    firstcommit

---
+++ app/src/main/java/com/example/restapione/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.restapione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    String JSON_STRING  = "{\"employee\":{\"name\":\"Master Android App\",\"downloads\":9000}}";
+    String name, downloads;
+    TextView appName,downloadsTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        appName = findViewById(R.id.name);
+        downloadsTextView = findViewById(R.id.salary);
+
+        try{
+            JSONObject obj = new JSONObject(JSON_STRING);
+            JSONObject employee = obj.getJSONObject("employee");
+
+            name = employee.getString("name");
+            downloads = employee.getString("downloads");
+
+            // set the name and salary
+            appName.setText("Name:"+name);
+            downloadsTextView.setText("Downloads:"+downloads+"Users");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
